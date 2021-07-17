62490bbb0e3822b5d234f4d43d33e785107ce2dd
munish710 <munish710@gmail.com>
2021-07-17 16:17:51 +0530

    added display questions functionality

--- src/App.js
+++ src/App.js
@@ -12,8 +12,33 @@ function App() {
   if (isLoading) {
     return <Loading />;
   }
-
-  return <main>Quiz app</main>;
+  const { question, incorrect_answers, correct_answer } = questions[index];
+  const answers = [...incorrect_answers, correct_answer];
+  return (
+    <main>
+      {/* <Modal /> */}
+      <section className="quiz">
+        <p className="correct-answers">
+          correct answers : {correct}/ {index}
+        </p>
+        <article className="container">
+          <h2 dangerouslySetInnerHTML={{ __html: question }} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className="answer-btn"
+                  dangerouslySetInnerHTML={{ __html: answer }}
+                />
+              );
+            })}
+          </div>
+        </article>
+        <button className="next-question">next question</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
