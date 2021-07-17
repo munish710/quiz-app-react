2fbd03c9157db11d3549942c5798783b35d6fdc8
munish710 <munish710@gmail.com>
2021-07-17 16:29:14 +0530

    added correct answer functionality

--- src/App.js
+++ src/App.js
@@ -5,8 +5,15 @@ import SetupForm from "./SetupForm";
 import Loading from "./Loading";
 import Modal from "./Modal";
 function App() {
-  const { waiting, isLoading, questions, index, correct, nextQuestion } =
-    useGlobalContext();
+  const {
+    waiting,
+    isLoading,
+    questions,
+    index,
+    correct,
+    nextQuestion,
+    checkAnswer,
+  } = useGlobalContext();
   if (waiting) {
     return <SetupForm />;
   }
@@ -31,6 +38,7 @@ function App() {
                   key={index}
                   className="answer-btn"
                   dangerouslySetInnerHTML={{ __html: answer }}
+                  onClick={() => checkAnswer(correct_answer === answer)}
                 />
               );
             })}
--- src/context.js
+++ src/context.js
@@ -56,6 +56,13 @@ const AppProvider = ({ children }) => {
     });
   };
 
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((oldState) => oldState + 1);
+    }
+    nextQuestion();
+  };
+
   useEffect(() => {
     fetchQuestions(tempUrl);
   }, []);
@@ -71,6 +78,7 @@ const AppProvider = ({ children }) => {
         error,
         isModalOpen,
         nextQuestion,
+        checkAnswer,
       }}
     >
       {children}
