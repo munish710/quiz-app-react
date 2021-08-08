5bfcba174254873c9872b52f559f9d7412506882
munish710 <munish710@gmail.com>
2021-08-08 19:28:07 +0530

    added error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import error_404 from "../assets/error_404.svg";
 const Error = () => {
-  return <div>Error page</div>;
+  return (
+    <section className="section page hero">
+      <div className="section-center hero-center">
+        <div className="hero-image-container">
+          <img src={error_404} alt="questions" className="hero-image" />
+        </div>
+        <div className="hero-info">
+          <h3>Page not Found</h3>
+          <p>Sorry, the page you are looking for is not available.</p>
+          <button className="btn">Go Back</button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Error;
