d7adda95c60c14c9f01c0af0099ee7163a9717e1
munish710 <munish710@gmail.com>
2021-08-09 01:05:28 +0530

    setup form style fixes

--- src/components/SetupForm.js
+++ src/components/SetupForm.js
@@ -5,65 +5,63 @@ const SetupForm = () => {
   const { quiz, handleChange, handleSubmit, error } = useGlobalContext();
 
   return (
-    <section className="section">
-      <div className="section-center quiz-small">
-        <form className="setup-form">
-          {/* amount */}
-          <div className="form-control">
-            <label htmlFor="amount">number of questions</label>
-            <input
-              type="number"
-              name="amount"
-              id="amount"
-              value={quiz.amount}
-              onChange={handleChange}
-              className="form-input"
-              min={1}
-              max={50}
-            />
-          </div>
-          {/* category */}
-          <div className="form-control">
-            <label htmlFor="category">Category</label>
-            <select
-              name="category"
-              id="category"
-              className="form-input"
-              value={quiz.category}
-              onChange={handleChange}
-            >
-              <option value="sports">sports</option>
-              <option value="history">history</option>
-              <option value="politics">politics</option>
-            </select>
-          </div>
+    <div>
+      <form className="setup-form">
+        {/* amount */}
+        <div className="form-control">
+          <label htmlFor="amount">number of questions</label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={quiz.amount}
+            onChange={handleChange}
+            className="form-input"
+            min={1}
+            max={50}
+          />
+        </div>
+        {/* category */}
+        <div className="form-control">
+          <label htmlFor="category">Category</label>
+          <select
+            name="category"
+            id="category"
+            className="form-input"
+            value={quiz.category}
+            onChange={handleChange}
+          >
+            <option value="sports">sports</option>
+            <option value="history">history</option>
+            <option value="politics">politics</option>
+          </select>
+        </div>
 
-          {/* difficulty */}
-          <div className="form-control">
-            <label htmlFor="difficulty">Choose Difficulty</label>
-            <select
-              name="difficulty"
-              id="difficulty"
-              className="form-input"
-              value={quiz.difficulty}
-              onChange={handleChange}
-            >
-              <option value="easy">easy</option>
-              <option value="medium">medium</option>
-              <option value="hard">hard</option>
-            </select>
-          </div>
-          {error && (
-            <p className="error">
-              Can't generate questions, please try different options
-            </p>
-          )}
-          <button type="submit" onClick={handleSubmit} className="submit-btn">
-            Start
-          </button>
-        </form>
-      </div>
-    </section>
+        {/* difficulty */}
+        <div className="form-control">
+          <label htmlFor="difficulty">Choose Difficulty</label>
+          <select
+            name="difficulty"
+            id="difficulty"
+            className="form-input"
+            value={quiz.difficulty}
+            onChange={handleChange}
+          >
+            <option value="easy">easy</option>
+            <option value="medium">medium</option>
+            <option value="hard">hard</option>
+          </select>
+        </div>
+        {error && (
+          <p className="error">
+            Can't generate questions, please try different options
+          </p>
+        )}
+        <button type="submit" onClick={handleSubmit} className="submit-btn">
+          Start Quiz
+        </button>
+      </form>
+    </div>
   );
 };
 
