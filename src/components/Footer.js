9b33073d92989411dae176dc3f53ea2c6b853370
munish710 <munish710@gmail.com>
2021-08-08 17:32:37 +0530

    added footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  AiOutlineGithub,
+  AiOutlineTwitter,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className=" footer">
+      <div className="section-center footer-center">
+        <p>
+          Made with <span className="code">&lt; / &gt;</span> by Munish Thakur
+        </p>
+        <div className="footer-icons">
+          <a href="" target="_blank" className="footer-icon">
+            <AiOutlineGithub />
+          </a>
+          <a href="" target="_blank" className="footer-icon">
+            <AiOutlineLinkedin />
+          </a>
+          <a href="" target="_blank" className="footer-icon">
+            <AiOutlineTwitter />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
